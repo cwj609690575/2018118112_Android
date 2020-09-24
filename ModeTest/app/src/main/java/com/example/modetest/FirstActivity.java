11736c940429320c9bb305482c7d482cbc27a713
DESKTOP-RSQU9B8 <609690575@qq.com>
2020-09-24 13:18:15 +0800

    Android作业3：四种模式

--- ModeTest/app/src/main/java/com/example/modetest/FirstActivity.java
+++ ModeTest/app/src/main/java/com/example/modetest/FirstActivity.java
@@ -1,8 +1,15 @@
 package com.example.modetest;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -17,9 +24,74 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(FirstActivity.this, "You clicked Button 1",
-                        Toast.LENGTH_SHORT).show();
+                //在活动中使用Toast
+                //Toast.makeText(FirstActivity.this, "You clicked Button 1", Toast.LENGTH_SHORT).show();
+
+                //显式Intent
+                //Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+
+                //隐式Intent
+                //Intent intent = new Intent("com.example.modetest.ACTION_START");
+                //intent.addCategory("com.example.modetest.MY_CATEGORY");
+                //百度
+                //Intent intent = new Intent(Intent.ACTION_VIEW);
+                //intent.setData(Uri.parse("http://www.baidu.com"));
+                //电话号码
+                //Intent intent = new Intent(Intent.ACTION_DIAL);
+                //intent.setData(Uri.parse("tel:10086"));
+                //向下一个活动传递数据
+                //String data = "Hello SecondActivity";
+                //Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                //intent.putExtra("extra_data", data);
+                //返回数据给上一个活动
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivityForResult(intent, 1);
+
+                //startActivity(intent);
             }
         });
+    }
+
+    /*
+     * 创建菜单选项
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    /*
+     * 定义菜单响应事件
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
+    }
+
+    /*
+     * 返回数据给上一个活动
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("FirstActivity", returnedData);
+                }
+                break;
+            default:
+        }
     }
 }
---
+++ ModeTest/app/src/main/java/com/example/modetest/SecondActivity.java
@@ -0,0 +1,35 @@
+package com.example.modetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+
+        //向下一个活动传递数据
+        //Intent intent1 = getIntent();
+        //String data = intent1.getStringExtra("extra_data");
+        //Log.d("SecondActivity", data);
+
+        //返回数据给上一个活动
+        Button button2 = (Button) findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("data_return", "Hello FirstActivity");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+}
