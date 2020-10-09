4b34e8a9eadf34c242629dcc079e2fadc350f5e5
DESKTOP-RSQU9B8 <609690575@qq.com>
2020-10-09 20:29:03 +0800

    update Homework 4

--- Homework 4/UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
+++ Homework 4/UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
@@ -7,8 +7,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +20,24 @@ public class MainActivity extends AppCompatActivity {
         Log.d("MainActivity", this.toString());
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.edit_text);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, MainActivity.class);
                 startActivity(intent);
+
+                switch (v.getId()){
+                    case R.id.button:
+                        String inputText = editText.getText().toString();
+                        Toast.makeText(MainActivity.this, inputText,
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+
+
             }
         });
     }
