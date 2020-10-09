db571051cc2ef4d5b6106a4d0dec45223857842e
DESKTOP-RSQU9B8 <609690575@qq.com>
2020-10-09 21:47:56 +0800

    update Homework 4

--- Homework 4/UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
+++ Homework 4/UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
@@ -8,36 +8,51 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editText;
+    private ImageView imageView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("MainActivity", this.toString());
+        //Log.d("MainActivity", this.toString());
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button);
         editText = (EditText) findViewById(R.id.edit_text);
+        imageView = (ImageView) findViewById(R.id.image_view);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MainActivity.class);
-                startActivity(intent);
+                //Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                //startActivity(intent);
 
                 switch (v.getId()){
                     case R.id.button:
                         String inputText = editText.getText().toString();
                         Toast.makeText(MainActivity.this, inputText,
                                 Toast.LENGTH_SHORT).show();
+
+                        imageView.setImageResource(R.drawable.img_2);
+
+                        if(progressBar.getVisibility() == View.GONE) {
+                            progressBar.setVisibility(View.VISIBLE);
+                        } else {
+                            progressBar.setVisibility(View.GONE);
+                        }
+                        int progress = progressBar.getProgress();
+                        progress = progress + 10;
+                        progressBar.setProgress(progress);
                         break;
                     default:
                         break;
                 }
-
-
             }
         });
     }
