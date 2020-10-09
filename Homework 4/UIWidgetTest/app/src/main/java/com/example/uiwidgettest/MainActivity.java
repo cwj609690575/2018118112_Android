bdbb8b8f153deeed039fb496cb7cd1e335d5c24e
DESKTOP-RSQU9B8 <609690575@qq.com>
2020-10-09 16:34:02 +0800

    update Homework 4

--- Homework 4/UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
+++ Homework 4/UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.uiwidgettest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("MainActivity", this.toString());
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
