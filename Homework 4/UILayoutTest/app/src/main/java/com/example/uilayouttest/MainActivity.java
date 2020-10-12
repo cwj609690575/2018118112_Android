05d0ef7e3a2bf40b24a8695815ad156b19a14c1e
DESKTOP-RSQU9B8 <609690575@qq.com>
2020-10-12 13:56:53 +0800

    update Homework 4

--- Homework 4/UILayoutTest/app/src/main/java/com/example/uilayouttest/MainActivity.java
+++ Homework 4/UILayoutTest/app/src/main/java/com/example/uilayouttest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.uilayouttest;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.hide();
+        }
     }
 }
